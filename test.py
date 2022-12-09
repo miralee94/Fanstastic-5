577499342b5acb60cc9896aba45d1b31bd903b14
Mira Lee <leemira1994@gmail.com>
2022-12-09 09:27:09 +0100

    strid

---
+++ test.py
@@ -0,0 +1,7 @@
+
+hero = {'name': 'riddare', 'initiativ': 5, 'tålighet': 9, 'atack': 6, 'smidighet': 4}
+monster = {'jättespindel': {'initiativ': 7, 'tålighet': 1, 'atack': 2, 'smidighet': 3}}
+
+
+hero_förmågor = [i for i in hero.values()]
+print(hero_förmågor)
---
+++ test_strid.py
@@ -0,0 +1,14 @@
+def test_role_dice():
+    pass
+
+def test_check_monster():
+    pass
+
+def test_check_hero():
+    pass
+
+def test_try_to_atack():
+    pass
+
+def test_try_to_fly():
+    pass
