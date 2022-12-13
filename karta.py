e9a8ba1188cefdf48d8e2135dfb5455f01ecd542
Saman Petfat <Saman.petfat@yh.nackademin.se>
2022-12-13 14:22:56 +0100

    kartändring

--- karta.py
+++ karta.py
@@ -1,3 +1,8 @@
+from skatter import *
+from karaktärer import *
+from strid import *
+
+
 class Board:
     def __init__(self, size):
         # Initialize the size of the board
@@ -7,6 +12,8 @@ class Board:
                       for j in range(self.size)]
         self.coord_x = 0
         self.coord_y = 0
+        self.monster = False
+        self.treasure = False
 
     def __str__(self):
         # This method is called when the object is printed,
@@ -68,35 +75,127 @@ class Board:
     def moving_topos(self):
         while True:
             self.vart_är_jag()
-            move = input("""\n
-                Enter Direction:
-                1. Left
-                2. Right
-                3. Up
-                4. Down \n""")
+            self.move = input("""\n
+                    Enter Direction:
+                    1. Left
+                    2. Right
+                    3. Up
+                    4. Down \n""")
             try:
-                if move == "1":
+                if self.move == "1":
                     self.my_board.set_cell((self.coord_x), self.coord_y, "X")
                     self.my_board.set_cell(
                         self.coord_x, (self.coord_y - 1), 'H')
                     print(self.my_board)
-                elif move == "2":
+                    self.create_a_room()
+                    # self.insert_monster_to_room()
+                elif self.move == "2":
                     self.my_board.set_cell((self.coord_x), self.coord_y, "X")
                     self.my_board.set_cell(
                         self.coord_x, (self.coord_y + 1), 'H')
                     print(self.my_board)
-                elif move == "3":
+                    self.create_a_room()
+                elif self.move == "3":
                     self.my_board.set_cell((self.coord_x), self.coord_y, "X")
                     self.my_board.set_cell(
                         (self.coord_x - 1), self.coord_y, 'H')
                     print(self.my_board)
-                elif move == "4":
+                    self.create_a_room()
+                elif self.move == "4":
                     self.my_board.set_cell((self.coord_x), self.coord_y, "X")
                     self.my_board.set_cell(
                         (self.coord_x + 1), self.coord_y, 'H')
                     print(self.my_board)
+                    self.create_a_room()
             except IndexError:
                 print("You can't go outside the map!")
+
+    # def insert_treasure_to_room(self):
+    #    if self.move == "1":
+    #        monster = Big_Spider()
+    #        skatt = Lösa_slantar()
+    #        if monster.monster_appears() == True:
+    #            skatt.treasure_appears() == False
+    #        else:
+    #            skatt.treasure_appears() == True
+
+    def create_a_room(self):
+        self.shuffle_monster()
+        self.shuffle_treasure()
+
+    def shuffle_treasure(self):
+        slantar = Lösa_slantar()
+        slantar.chance_of_appearance()
+        if slantar.chance_of_appearance() is True:
+            self.treasure is True
+            print("Lösa Slantar finns")
+        else:
+            print('Ingen skatt')
+
+        pengapung = Lösa_slantar()
+        pengapung.chance_of_appearance()
+        if pengapung.chance_of_appearance() is True:
+            self.treasure is True
+            print('Pengapung finns.')
+        else:
+            print('Ingen skatt')
+
+        guldsmycket = Guldsmycket()
+        guldsmycket.chance_of_appearance()
+        if guldsmycket.chance_of_appearance() is True:
+            self.treasure is True
+            print('Guldsmycket finns')
+        else:
+            print('Ingen skatt')
+
+        ädelsten = Ädelsten()
+        ädelsten.chance_of_appearance()
+        if ädelsten.chance_of_appearance() is True:
+            self.treasure is True
+            print('Ädelsten finns')
+        else:
+            print('Ingen skatt')
+
+        liten_kista = Liten_skattkista()
+        liten_kista.chance_of_appearance()
+        if liten_kista.chance_of_appearance() is True:
+            self.treasure is True
+            print('Liten Kista finns')
+        else:
+            print('Ingen skatt')
+
+    def shuffle_monster(self):
+        spider = Big_Spider()
+        spider.chance_of_appearance()
+        if spider.chance_of_appearance() is True:
+            self.monster is True
+            print('Big spider finns i rummet')
+        else:
+            print('ingen Big spider')
+
+        skeleton = Skeleton()
+        skeleton.chance_of_appearance()
+        if skeleton.chance_of_appearance() is True:
+            self.monster is True
+            print('Skeleton finns i rummet')
+        else:
+            print('ingen Skeleton')
+
+        troll = Troll()
+        troll.chance_of_appearance()
+        if troll.chance_of_appearance() is True:
+            self.monster is True
+            print('Troll finns i rummet')
+        else:
+            print('ingen Troll')
+
+        orc = Orc()
+        orc.chance_of_appearance()
+        if orc.chance_of_appearance() is True:
+            self.monster is True
+            print('Orc finns i rummet')
+        else:
+            print('ingen Orc')
 
 
 def main():
