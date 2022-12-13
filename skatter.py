45de108e01bba23f25ae5d831bd1c3d6d2b2f5c5
David Sundgren <sundgren.david@gmail.com>
2022-12-13 14:37:33 +0100

    added skatter

---
+++ skatter.py
@@ -0,0 +1,52 @@
+import random
+
+
+class Skatter:
+    def __init__(self, värde, vanlighet):
+        self.värde = värde
+        self.vanlighet = vanlighet  # kanske tas bort.
+
+
+class Lösa_slantar(Skatter):
+    def __init__(self):
+        super().__init__(2, 40)
+
+    @classmethod
+    def chance_of_appearance(cls):
+        return random.random() <= 0.4
+
+
+class Pengapung(Skatter):
+    def __init__(self):
+        super().__init__(6, 20)
+
+    @classmethod
+    def chance_of_appearance(cls):
+        return random.random() <= 0.2
+
+
+class Guldsmycket(Skatter):
+    def __init__(self):
+        super().__init__(10, 15)
+
+    @classmethod
+    def chance_of_appearance(cls):
+        return random.random() <= 0.15
+
+
+class Ädelsten(Skatter):
+    def __init__(self):
+        super().__init__(14, 10)
+
+    @classmethod
+    def chance_of_appearance(cls):
+        return random.random() <= 0.1
+
+
+class Liten_skattkista(Skatter):
+    def __init__(self):
+        super().__init__(20, 5)
+
+    @classmethod
+    def chance_of_appearance(cls):
+        return random.random() <= 0.05
