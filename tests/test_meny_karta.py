1e4691fac6854b20493b33d61315bee0231da98c
fradop <hugogoransson1@gmail.com>
2022-12-19 15:10:56 +0100

    lite tester

---
+++ tests/test_meny_karta.py
@@ -0,0 +1,33 @@
+from meny import Menu
+m = Menu()
+from karta import Board
+
+
+def test_choice_of_hero():
+    assert Menu.Choice_of_hero == """
+    Choose your hero!
+1. The Knight
+2. The Wizard
+3. The Thief
+Type b or B to go back to the main menu"""
+
+
+def test_main_menu_text():
+    assert Menu.MAIN_MENU_TEXT == """
+    Welcome!
+1. Start a new game
+2. Load game
+
+Type q or Q to exit the program"""
+
+def test_menu_commands():
+    assert Menu.menu_commands(Menu, "5") == None
+
+
+def test_submenu_exit():
+    b = Board(4)
+    assert b.subMenu_Exit() == None
+    b = Board(5)
+    assert b.subMenu_Exit() == None
+    b = Board(8)
+    assert b.subMenu_Exit() == None
